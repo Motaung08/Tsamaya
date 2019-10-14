f5b5bbc5a8cae7dbe084c74f8d47375b494aa73c
Motaung08 <50378344+Motaung08@users.noreply.github.com>
2019-10-15 01:47:23 +0200

    Update LoginActivityTest.java

--- app/src/androidTest/java/com/example/codingmountain/googlemap_clone_bottomsheetlayout/LoginActivityTest.java
+++ app/src/androidTest/java/com/example/codingmountain/googlemap_clone_bottomsheetlayout/LoginActivityTest.java
@@ -60,6 +60,11 @@ public class LoginActivityTest {
 
         String password = "123456";
         String username = "alec@gmail.com";
+        
+        onView(withId(R.id.input_password)).perform(typeText(password));
+        closeSoftKeyboard();
+        Thread.sleep(1000);
+        
         loginActivity._emailText.setText("alec@gmail.com");
         
         
@@ -67,9 +72,7 @@ public class LoginActivityTest {
         closeSoftKeyboard();
         Thread.sleep(1000);
 
-        onView(withId(R.id.input_password)).perform(typeText(password));
-        closeSoftKeyboard();
-        Thread.sleep(1000);
+        
         
         onView(withId(R.id.btn_login)).perform(click());
 
